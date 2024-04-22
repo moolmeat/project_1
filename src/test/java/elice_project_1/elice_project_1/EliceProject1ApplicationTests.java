2972c075b30205f0a95f22a7d67b06022b693ab6
[Cloud3] 우문식 <woomoon1107@gmail.com>
2024-04-22 11:40:53 +0900

    first commit

---
+++ src/test/java/elice_project_1/elice_project_1/EliceProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package elice_project_1.elice_project_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliceProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
